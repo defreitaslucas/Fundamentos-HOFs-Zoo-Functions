4d235e2d9085a57f198529871adcfbc1973e133d
LucasTapa <lucas.dfa@live.com>
2022-01-06 17:45:40 -0300

    requisito 3 feito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  const funcionario = employees.find((elemento) =>
+    elemento.firstName === employeeName || elemento.lastName === employeeName);
+  if (!funcionario) {
+    return {};
+  }
+  return funcionario;
 }
 
 module.exports = getEmployeeByName;
