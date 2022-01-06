6a7d7a380c44d3c369039b40403acb1a860df2a8
LucasTapa <lucas.dfa@live.com>
2022-01-06 17:03:15 -0300

    requisito 2 feito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
+
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  return species.filter((elemento) => elemento.name === animal)
+    .reduce((acc, elemento) => elemento.residents.every((item) => item.age >= age), 0);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  // chamei a função map nos ids para que crie um array contendo os todos os ids passados e fiz um find em species para preencher o array map com os elementos comparando id ao parametro item
+  return ids.map((item) => species.find((elemento) => elemento.id === item));
 }
 
 module.exports = getSpeciesByIds;
