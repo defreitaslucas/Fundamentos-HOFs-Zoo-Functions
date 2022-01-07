f7dc739758836cd403d0306de2f908abd92e8a81
LucasTapa <lucas.dfa@live.com>
2022-01-07 17:55:51 -0300

    requisito 9 refatorado

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -4,10 +4,10 @@ const data = require('../data/zoo_data');
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
   const responsavel = employees.filter((elemento) => elemento.id === id);
-  const responsavelAnimal = responsavel.map((elemento) => elemento.responsibleFor[0]);
-  const especie = species.find((elemento) => elemento.id === responsavelAnimal[0]);
+  const idAnimal = responsavel[0].responsibleFor[0];
+  const especie = species.find((elemento) => elemento.id === idAnimal);
   const animais = especie.residents.sort((a, b) => b.age - a.age);
-  const primeiro = animais.find((elemento) => elemento);
+  const primeiro = animais[0];
   const resultado = Object.values(primeiro).map((item) => item);
   return resultado;
 }
