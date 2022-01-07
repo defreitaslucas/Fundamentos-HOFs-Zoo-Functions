6d4975c0fd4b8355048df1596807ec7ca83f320a
LucasTapa <lucas.dfa@live.com>
2022-01-07 15:56:01 -0300

    teste requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,36 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+
+const objeto = {
+  child: 0,
+  adult: 0,
+  senior: 0,
+};
 
 function countEntrants(entrants) {
   // seu código aqui
+  return entrants.reduce((acc, item) => {
+    if (item.age < 18) {
+      acc.child += 1;
+    } else if (item.age >= 18 && item.age < 50) {
+      acc.adult += 1;
+    } else {
+      acc.senior += 1;
+    }
+    return acc;
+  }, objeto);
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const retornoCount = countEntrants(entrants);
+  const variavel = (retornoCount.child * prices.child)
+  + (retornoCount.adult * prices.adult) + (retornoCount.senior * prices.senior);
+  console.log(variavel);
+  return variavel;
 }
 
 module.exports = { calculateEntry, countEntrants };
