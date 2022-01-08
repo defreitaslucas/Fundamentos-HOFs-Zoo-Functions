8ffc6a2e91666d462d50b513a0635ac10535155b
LucasTapa <lucas.dfa@live.com>
2022-01-07 21:16:12 -0300

    requisito 7 feito

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,23 +1,56 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function createAnimalList(options, specie) {
-  return specie.residents
-    .filter((animal) => (options.sex && animal.sex === options.sex) || (!options.sex))
-    .map((animal) => animal.name);
+function categorizaLocation() {
+  const animal = species.reduce((acc, elemento) => {
+    if (elemento.location === 'NE') {
+      acc.NE.push(elemento.name);
+    } else if (elemento.location === 'NW') {
+      acc.NW.push(elemento.name);
+    } else if (elemento.location === 'SE') {
+      acc.SE.push(elemento.name);
+    } else {
+      acc.SW.push(elemento.name);
+    }
+    return acc;
+  }, { NE: [], NW: [], SE: [], SW: [] });
+  return animal;
+}
+
+function includeTrue(array, options) {
+  return array.reduce((acc, elemento) => {
+    let nomeAnimais;
+    if (options.sex) {
+      nomeAnimais = elemento.residents.filter((item) => item.sex === options.sex)
+        .map((item) => item.name);
+    } else {
+      nomeAnimais = elemento.residents.map((item) => item.name);
+    }
+    if (options.sorted) {
+      nomeAnimais = nomeAnimais.sort();
+    }
+    acc.push({ [elemento.name]: nomeAnimais });
+    return acc;
+  }, []);
 }
 
 function getAnimalMap(options = { includeNames: false, sorted: false, sex: false }) {
-  const result = data.species.reduce((obj, specie) => {
-    if (options.includeNames) {
-      const animalList = createAnimalList(options, specie);
-      if (options.sorted) animalList.sort();
-      obj[specie.location].push({ [specie.name]: animalList });
-    } else {
-      obj[specie.location].push(specie.name);
-    }
-    return obj;
-  }, { NE: [], NW: [], SE: [], SW: [] });
-  return result;
+  if (!options.includeNames) {
+    const location = categorizaLocation();
+    return location;
+  }
+  const specieNe = species.filter((elemento) => elemento.location === 'NE');
+  const specieNw = species.filter((elemento) => elemento.location === 'NW');
+  const specieSe = species.filter((elemento) => elemento.location === 'SE');
+  const specieSw = species.filter((elemento) => elemento.location === 'SW');
+  const resultado = {
+    NE: includeTrue(specieNe, options),
+    NW: includeTrue(specieNw, options),
+    SE: includeTrue(specieSe, options),
+    SW: includeTrue(specieSw, options),
+  };
+  return resultado;
 }
+console.log(JSON.stringify(getAnimalMap({ includeNames: true, sex: 'female' })));
 
 module.exports = getAnimalMap;
